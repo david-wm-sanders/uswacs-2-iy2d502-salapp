24ba72b51fe01fda1a027ff73e49dd52216893bf
David Sanders <david.wm.sanders@gmail.com>
2019-01-03 04:30:35 +0000

    Add basic template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Hello World!"
+    user = {"username": "dwms"}
+    quotes = [{"email": "d@w.local", "forename": "D", "surname": "W"},
+              {"email": "m@s.local", "forename": "M", "surname": "S"}]
+    return render_template("index.html", user=user, quotes=quotes)
+    # return render_template("index.html", title="Home", user=user)
