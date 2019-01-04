9bfa38db7b812217c9055b1fad0f53eb6d0fd925
David Sanders <david.wm.sanders@gmail.com>
2019-01-04 06:40:44 +0000

    Include links properly with url_for

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, url_for, flash, redirect
 from app import app
 from app.forms import QuoteForm
 
@@ -13,9 +13,13 @@ def index():
     return render_template("index.html", user=user, quotes=quotes)
 
 @app.route("/get-a-quote", methods=["GET", "POST"])
-def quote():
+def get_a_quote():
     form = QuoteForm()
     if form.validate_on_submit():
         flash("Quote request received!")
-        # return redirect("/thanks")
+        # return redirect(url_for("thanks"))
     return render_template("get_quote.html", title="Get a Quote!", form=form)
+
+@app.route("/about")
+def about():
+    return "<p>Under construction by dwms</p>"
