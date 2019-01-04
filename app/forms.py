6aca2556f4d09b243926f4079ba79b81d6567f89
David Sanders <david.wm.sanders@gmail.com>
2019-01-04 05:55:15 +0000

    Add get-a-quote route, page, and quotes form

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class QuoteForm(FlaskForm):
+    email = StringField("Email:", validators=[DataRequired(), Email()])
+    password = PasswordField("Password:", validators=[DataRequired()])
+    forename = StringField("Forename:", validators=[DataRequired()])
+    surname = StringField("Surname:", validators=[DataRequired()])
+    telephone = StringField("Telephone:", validators=[DataRequired()])
+    account_number = StringField("Account Number:", validators=[DataRequired()])
+    sort_code = StringField("Sort Code:", validators=[DataRequired()])
+    address = StringField("Address:", validators=[DataRequired()])
+    # town = StringField("Town:", validators=[DataRequired()])
+    postcode = StringField("Postcode:", validators=[DataRequired()])
+    submit = SubmitField("Request Quote!")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import QuoteForm
 
 @app.route("/")
 @app.route("/index")
 def index():
+    # Demonstrate app logging
+    app.logger.info(f"Request received for '/'!")
     user = {"username": "dwms"}
     quotes = [{"email": "d@w.local", "forename": "D", "surname": "W"},
               {"email": "m@s.local", "forename": "M", "surname": "S"}]
     return render_template("index.html", user=user, quotes=quotes)
-    # return render_template("index.html", title="Home", user=user)
+
+@app.route("/get-a-quote", methods=["GET", "POST"])
+def quote():
+    form = QuoteForm()
+    if form.validate_on_submit():
+        flash("Quote request received!")
+        # return redirect("/thanks")
+    return render_template("get_quote.html", title="Get a Quote!", form=form)
