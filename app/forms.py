c09722ac9578d4c3cc90a8ce3cc4e4caf5699e35
David Sanders <david.wm.sanders@gmail.com>
2019-01-10 00:17:43 +0000

    Add custom ModelStringField and ModelLengthValidator

--- app/forms.py
+++ app/forms.py
@@ -2,47 +2,62 @@ from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import ValidationError
 
+from app import app
 from app.models import Quote
 
 
-class QuoteForm(FlaskForm):
-    # Define internal variables that grab the column length from the Quote model
-    # Figure out a way to refactor this - try to subclass FlaskForm perhaps?
-    _maxlen_email = Quote.email.property.columns[0].type.length
-    _maxlen_forename = Quote.forename.property.columns[0].type.length
-    _maxlen_surname = Quote.surname.property.columns[0].type.length
-    _maxlen_telephone = Quote.telephone.property.columns[0].type.length
-    _maxlen_account_number = Quote.account_number.property.columns[0].type.length
-    _maxlen_sort_code = Quote.sort_code.property.columns[0].type.length
-    _maxlen_address = Quote.address.property.columns[0].type.length
-    _maxlen_town = Quote.town.property.columns[0].type.length
-    _maxlen_postcode = Quote.postcode.property.columns[0].type.length
+class ModelLengthValidator:
+    """Validates field length from SQLAlchemy property type length"""
+    def __init__(self, model_field, minimum_length=0):
+        self._min_length = minimum_length
+        self._max_length = model_field.property.columns[0].type.length
 
+    def __call__(self, form, field):
+        field_data_length = field.data and len(field.data) or 0
+        if field_data_length < self._min_length:
+            return ValidationError(f"Field cannot be less than {self._min_length} characters long.")
+        if field_data_length > self._max_length:
+            raise ValidationError(f"Field cannot be more than {self._max_length} characters long.")
+
+
+class ModelStringField(StringField):
+    def __init__(self, label="", validators=None, model_field=None, **kwargs):
+        self._max_length = model_field.property.columns[0].type.length
+        super(StringField, self).__init__(label, validators, **kwargs)
+
+    def __call__(self, **kwargs):
+        if "maxlength" not in kwargs:
+            kwargs["maxlength"] = self._max_length
+        return super(StringField, self).__call__(**kwargs)
+
+
+class QuoteForm(FlaskForm):
     # TODO: Add better ALERT (warning) logging on validation failure, currently handled in route
     # TODO: SQL injection check - validate for possible SQL injection strings, ALERT, and reject with funny message
     # TODO: Password complexity and (basic) not in wordlist validation for relevant forms
 
     # Define form fields
-    # TODO: Derive custom LengthedStringField that adds length validation and sets HTML maxlength attribute
-    email = StringField("Email", validators=[DataRequired(), Email(), Length(max=_maxlen_email)],
-                        render_kw={"maxlength": _maxlen_email})
-    forename = StringField("Forename", validators=[DataRequired(), Length(max=_maxlen_forename)],
-                           render_kw={"maxlength": _maxlen_forename})
-    surname = StringField("Surname", validators=[DataRequired(), Length(max=_maxlen_surname)],
-                          render_kw={"maxlength": _maxlen_surname})
-    telephone = StringField("Telephone", validators=[DataRequired(), Length(max=_maxlen_telephone)],
-                            render_kw={"maxlength": _maxlen_telephone})
-    account_number = StringField("Account Number", validators=[DataRequired(), Length(max=_maxlen_account_number)],
-                                 render_kw={"maxlength": _maxlen_account_number})
-    sort_code = StringField("Sort Code", validators=[DataRequired(), Length(max=_maxlen_sort_code)],
-                            render_kw={"maxlength": _maxlen_sort_code})
-    address = StringField("Address", validators=[DataRequired(), Length(max=_maxlen_address)],
-                          render_kw={"maxlength": _maxlen_address})
-    town = StringField("Town", validators=[DataRequired(), Length(max=_maxlen_town)],
-                       render_kw={"maxlength": _maxlen_town})
-    postcode = StringField("Postcode", validators=[DataRequired(), Length(max=_maxlen_postcode)],
-                           render_kw={"maxlength": _maxlen_postcode})
+    email = ModelStringField("Email", validators=[DataRequired(), Email(), ModelLengthValidator(Quote.email)],
+                             model_field=Quote.email)
+    forename = ModelStringField("Forename", validators=[DataRequired(), ModelLengthValidator(Quote.forename)],
+                                model_field=Quote.forename)
+    surname = ModelStringField("Surname", validators=[DataRequired(), ModelLengthValidator(Quote.surname)],
+                               model_field=Quote.surname)
+    telephone = ModelStringField("Telephone", validators=[DataRequired(), ModelLengthValidator(Quote.telephone)],
+                                 model_field=Quote.telephone)
+    account_number = ModelStringField("Account Number", validators=[DataRequired(),
+                                                                    ModelLengthValidator(Quote.account_number)],
+                                      model_field=Quote.account_number)
+    sort_code = ModelStringField("Sort Code", validators=[DataRequired(), ModelLengthValidator(Quote.sort_code)],
+                                 model_field=Quote.sort_code)
+    address = ModelStringField("Address", validators=[DataRequired(), ModelLengthValidator(Quote.address)],
+                               model_field=Quote.address)
+    town = ModelStringField("Town", validators=[DataRequired(), ModelLengthValidator(Quote.town)],
+                            model_field=Quote.town)
+    postcode = ModelStringField("Postcode", validators=[DataRequired(), ModelLengthValidator(Quote.postcode)],
+                                model_field=Quote.postcode)
     # DEBUG: disable recaptcha for development and testing, use a HiddenField to represent it in the template
     # recaptcha = RecaptchaField()
     recaptcha = HiddenField()
