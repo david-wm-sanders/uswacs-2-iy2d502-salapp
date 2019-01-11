cde2ccbe38dd288a9812a61c7d02e1b943fb08e8
David Sanders <david.wm.sanders@gmail.com>
2019-01-11 04:26:05 +0000

    Add pydocstyle

--- pystyleproj.py
+++ pystyleproj.py
@@ -13,6 +13,7 @@ Options:
     -d      Include pydocstyle infractions
     -v      Show verbose infraction detail
 """
+import configparser
 import itertools
 import platform
 import subprocess
@@ -31,25 +32,46 @@ def pyxstyle_path(x, venv_dir="venv"):
     return [str(path_here / f"{venv_dir}/{bin_dir}/py{x}style{extension}")]
 
 
-def make_pyxstyle_command(x, opts=None, verbose=False):
+def make_pyxstyle_command(x, p, opts=None, verbose=False):
     opts = opts if opts else []
     verbose = ["-v"] if verbose else []
-    return list(itertools.chain(pyxstyle_path(x), opts, verbose, str(path_here)))
+    return list(itertools.chain(pyxstyle_path(x), opts, verbose, [str(p)]))
 
 
 args = docopt(__doc__)
+cmds = []
 if args["-c"]:
-    cmds = [make_pyxstyle_command("code", ["--statistics", "-qq"], verbose=args["-v"])]
+    cmds.append(make_pyxstyle_command("code", path_here, ["--statistics", "-qq"], verbose=args["-v"]))
 elif args["-s"]:
-    cmds = [make_pyxstyle_command("code", ["--show-source"], verbose=args["-v"])]
+    cmds.append(make_pyxstyle_command("code", path_here, ["--show-source"], verbose=args["-v"]))
 elif args["-r"]:
-    cmds = [make_pyxstyle_command("code", ["--show-source", "--show-pep8"], verbose=args["-v"])]
+    cmds.append(make_pyxstyle_command("code", path_here, ["--show-source", "--show-pep8"], verbose=args["-v"]))
 elif args["-f"]:
-    cmds = [make_pyxstyle_command("code", ["--first"], verbose=args["-v"])]
+    cmds.append(make_pyxstyle_command("code", path_here, ["--first"], verbose=args["-v"]))
 else:
-    cmds = [make_pyxstyle_command("code", verbose=args["-v"])]
+    cmds.append(make_pyxstyle_command("code", path_here, verbose=args["-v"]))
+
+if args["-d"]:
+    # Load excludes from tox.ini
+    excludes = ["__pycache__"]
+    tox_config = configparser.ConfigParser()
+    tox_config.read(str(path_here / "tox.ini"))
+    if "pycodestyle" in tox_config:
+        if "exclude" in tox_config["pycodestyle"]:
+            excludes.extend(tox_config["pycodestyle"]["exclude"].split(","))
+            print(f"Excluding {', '.join(excludes)}")
+
+    # Find folders and files at top level
+    folders = (x for x in path_here.iterdir() if x.is_dir() and x.name not in excludes)
+    things = list(itertools.chain(path_here.glob("*.py"), folders))
+    for thing in things:
+        if args["-c"]:
+            cmds.append(make_pyxstyle_command("doc", thing, ["--count"], verbose=args["-v"]))
+        else:
+            cmds.append(make_pyxstyle_command("doc", thing, verbose=args["-v"]))
 
 for cmd in cmds:
     if args["-v"]:
         print(f"Running {' '.join(arg for arg in cmd)}")
     subprocess.run(cmd)
+    print()
