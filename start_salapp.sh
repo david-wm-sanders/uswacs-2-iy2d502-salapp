731860b1d234d26bc93ad5d10f965c289a673821
David Sanders <david.wm.sanders@gmail.com>
2019-02-26 19:15:49 +0000

    Expose salapp container on 80 and 443

--- start_salapp.sh
+++ start_salapp.sh
@@ -1,1 +1,1 @@
-docker run --name salapp -d -p 8000:5000 --rm -e FLASK_ENV=development -e SECRET_KEY=$1 -e RECAPTCHA_PUBLIC_KEY=$2 -e RECAPTCHA_PRIVATE_KEY=$3  --link mysql:dbserver -e DATABASE_URI=mysql+pymysql://salapp:debugpw54321@dbserver/salapp salapp:latest
+docker run --name salapp -d -p 80:5000 -p 443:5000 --rm -e FLASK_ENV=development -e SECRET_KEY=$1 -e RECAPTCHA_PUBLIC_KEY=$2 -e RECAPTCHA_PRIVATE_KEY=$3  --link mysql:dbserver -e DATABASE_URI=mysql+pymysql://salapp:debugpw54321@dbserver/salapp salapp:latest
